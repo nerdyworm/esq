7632481326835509e20a0419aa114b702f88a93e
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2016-08-01 18:34:12 -0700

    do not run in iex shell

--- lib/esq/supervisor.ex
+++ lib/esq/supervisor.ex
@@ -6,6 +6,7 @@ defmodule Esq.Supervisor do
   end
 
   def init([queue, options]) do
+    running = Keyword.get(options, :dispatcher, !iex?)
     pool = Keyword.get(options, :pool, Esq.WorkerPool)
     workers = Keyword.get(options, :workers, 10)
 
@@ -16,11 +17,19 @@ defmodule Esq.Supervisor do
       {:max_overflow, 0}
     ]
 
-    children = [
-      :poolboy.child_spec(pool, poolboy_config, []),
-      worker(Esq.Dispatch, [queue, pool]),
-    ]
+    children = if running do
+      [
+        :poolboy.child_spec(pool, poolboy_config, []),
+        worker(Esq.Dispatch, [queue, pool]),
+      ]
+    else
+      []
+    end
 
     supervise(children, strategy: :one_for_one)
+  end
+
+  def iex? do
+    Code.ensure_loaded?(IEx) and IEx.started?
   end
 end
